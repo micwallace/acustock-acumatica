4ce40a48a74ebe52eee10a8c7c0606818664278e
Michael Wallace <michaelw@zhik.com>
2021-10-12 14:29:55 +1100

    Acumatica 2021.1 compatibility.

--- AcuStock/AcuStockConfig.cs
+++ AcuStock/AcuStockConfig.cs
@@ -13,7 +13,7 @@ namespace AcuStock
     {
       [PXString(16)]
       [PXUIField(DisplayName="Version")]
-      [PXDefault(TypeCode.String, "1.2.4")]
+      [PXDefault(TypeCode.String, "1.3.0")]
       public virtual string Version { get; set; }
       public abstract class version : IBqlField { }
     }
